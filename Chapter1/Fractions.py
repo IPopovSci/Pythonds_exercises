edaba33128b47358a8d4b4c39160e0afe6f639c9
IvanPopAudio <72165662+IvanPopAudio@users.noreply.github.com>
2023-05-15 15:17:43 -0400

    Completed Fractions 1.13.1.3

---
+++ Chapter1/Fractions.py
@@ -0,0 +1,68 @@
+'''To make sure you understand how operators are implemented in Python classes, and how to properly write methods,
+write some methods to implement *, /, and - . Also implement comparison operators > and <'''
+
+def gcd(m,n):
+    while m%n != 0:
+        oldm = m
+        oldn = n
+
+        m = oldn
+        n = oldm%oldn
+    return n
+
+class Fraction:
+     def __init__(self,top,bottom):
+         self.num = top
+         self.den = bottom
+
+     def __str__(self):
+         return str(self.num)+"/"+str(self.den)
+
+     def show(self):
+         print(self.num,"/",self.den)
+
+     def __add__(self,otherfraction):
+         newnum = self.num*otherfraction.den + \
+                      self.den*otherfraction.num
+         newden = self.den * otherfraction.den
+         common = gcd(newnum,newden)
+         return Fraction(newnum//common,newden//common)
+
+     def __eq__(self, other):
+         firstnum = self.num * other.den
+         secondnum = other.num * self.den
+
+         return firstnum == secondnum
+     def __mul__(self, other):
+         newnum = self.num*other.num
+         newden = self.den*other.den
+         common = gcd(newnum, newden)
+         return Fraction(newnum//common,newden//common)
+     def __truediv__(self, other):
+         newnum = self.num*other.den
+         newden = self.den * other.num
+         common = gcd(newnum, newden)
+         return Fraction(newnum // common, newden // common)
+     def __sub__(self, other):
+         newnum = (self.num*other.den - self.den*other.num)
+         newden = self.den*other.den
+         common = gcd(newnum, newden)
+         return Fraction(newnum // common, newden // common)
+     def __lt__(self, other):
+         a_newnum = self.num*other.den
+         b_newnum = other.num*self.den
+         return a_newnum<b_newnum
+     def __gt__(self, other):
+         a_newnum = self.num*other.den
+         b_newnum = other.num*self.den
+         return a_newnum>b_newnum
+
+x = Fraction(1,2)
+y = Fraction(2,3)
+print(x+y)
+print(x == y)
+print(x*y)
+print(x/y)
+print(x-y)
+print(x<y)
+print(x>y)
