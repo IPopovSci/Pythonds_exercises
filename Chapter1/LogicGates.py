94f7e099890cc62a650c67b0666d557cd3ca8b3b
IvanPopAudio <72165662+IvanPopAudio@users.noreply.github.com>
2023-05-16 14:42:09 -0400

    Implement HalfAdder

--- Chapter1/LogicGates.py
+++ Chapter1/LogicGates.py
@@ -8,11 +8,13 @@ Make sure to use some of your new gates in the simulation.
 '''
 '''Q.10 Research other types of gates that exist (such as NAND, NOR, and XOR). 
 Add them to the circuit hierarchy. How much additional coding did you need to do?'''
+'''Q.11 The most simple arithmetic circuit is known as the half-adder. 
+Research the simple half-adder circuit. Implement this circuit.'''
 
 
 class LogicGate:
 
-    def __init__(self,n):
+    def __init__(self, n):
         self.name = n
         self.output = None
 
@@ -26,7 +28,7 @@ class LogicGate:
 
 class BinaryGate(LogicGate):
 
-    def __init__(self,n):
+    def __init__(self, n):
         super(BinaryGate, self).__init__(n)
 
         self.pinA = None
@@ -34,17 +36,21 @@ class BinaryGate(LogicGate):
 
     def getPinA(self):
         if self.pinA == None:
-            return int(input("Enter Pin A input for gate "+self.getLabel()+"-->"))
-        else:
+            return int(input("Enter Pin A input for gate " + self.getLabel() + "-->"))
+        elif isinstance(self.pinA, Connector):
             return self.pinA.getFrom().getOutput()
+        else:  # If the pin is already set to a value
+            return self.pinA
 
     def getPinB(self):
         if self.pinB == None:
-            return int(input("Enter Pin B input for gate "+self.getLabel()+"-->"))
-        else:
+            return int(input("Enter Pin B input for gate " + self.getLabel() + "-->"))
+        elif isinstance(self.pinB,Connector):
             return self.pinB.getFrom().getOutput()
+        else: #If the pin is already set to a value
+            return self.pinB
 
-    def setNextPin(self,source):
+    def setNextPin(self, source):
         if self.pinA == None:
             self.pinA = source
         else:
@@ -52,50 +58,110 @@ class BinaryGate(LogicGate):
                 self.pinB = source
             else:
                 print("Cannot Connect: NO EMPTY PINS on this gate")
+    def setInput(self):
+        self.pinA = int(input("Enter Pin A input for gate " + self.getLabel() + "-->"))
+        self.pinB = int(input("Enter Pin B input for gate " + self.getLabel() + "-->"))
+
+class InputGate(BinaryGate):
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+        self.pinA = None
+        self.pinB = None
+    def performGateLogic(self):
+        if self.pinA == None:
+            self.pinA = int(input("Enter Pin A input for gate " + self.getLabel() + "-->"))
+            return self.pinA
+        if self.pinB == None:
+            self.pinB = int(input("Enter Pin B input for gate " + self.getLabel() + "-->"))
+            return self.pinB
 
 
 class AndGate(BinaryGate):
 
-    def __init__(self,n):
-        BinaryGate.__init__(self,n)
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
 
     def performGateLogic(self):
 
         a = self.getPinA()
         b = self.getPinB()
-        if a==1 and b==1:
+        if a == 1 and b == 1:
             return 1
         else:
             return 0
+
+
+# question 10
+class NandGate(BinaryGate):
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 and b == 1:
+            return 0
+        else:
+            return 1
+
+
+class NorGate(BinaryGate):
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 or b == 1:
+            return 0
+        else:
+            return 1
+
+
+class XorGate(BinaryGate):
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == b:
+            return 0
+        else:
+            return 1
+
 
 class OrGate(BinaryGate):
 
-    def __init__(self,n):
-        BinaryGate.__init__(self,n)
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
 
     def performGateLogic(self):
 
         a = self.getPinA()
         b = self.getPinB()
-        if a ==1 or b==1:
+        if a == 1 or b == 1:
             return 1
         else:
             return 0
 
+
+
 class UnaryGate(LogicGate):
 
-    def __init__(self,n):
-        LogicGate.__init__(self,n)
+    def __init__(self, n):
+        LogicGate.__init__(self, n)
 
         self.pin = None
 
     def getPin(self):
         if self.pin == None:
-            return int(input("Enter Pin input for gate "+self.getLabel()+"-->"))
+            return int(input("Enter Pin input for gate " + self.getLabel() + "-->"))
         else:
             return self.pin.getFrom().getOutput()
 
-    def setNextPin(self,source):
+    def setNextPin(self, source):
         if self.pin == None:
             self.pin = source
         else:
@@ -104,14 +170,20 @@ class UnaryGate(LogicGate):
 
 class NotGate(UnaryGate):
 
-    def __init__(self,n):
-        UnaryGate.__init__(self,n)
+    def __init__(self, n):
+        UnaryGate.__init__(self, n)
 
     def performGateLogic(self):
         if self.getPin():
             return 0
         else:
             return 1
+
+class EmptyUniGate(UnaryGate):
+    def __init__(self, n):
+        UnaryGate.__init__(self, n)
+    def performGateLogic(self):
+        return self.getPin()
 
 
 class Connector:
@@ -124,19 +196,31 @@ class Connector:
 
     def getFrom(self):
         return self.fromgate
-
     def getTo(self):
         return self.togate
+#Q11
+
+class HalfAdder:
+    def __init__(self):
+        self.sumout = XorGate('Sum')
+        self.carry = AndGate('Carry')
+
+        # self.sumout.setNextPin(self)
 
 
-def main():
-   g1 = AndGate("G1")
-   g2 = AndGate("G2")
-   g3 = OrGate("G3")
-   g4 = NotGate("G4")
-   c1 = Connector(g1,g3)
-   c2 = Connector(g2,g3)
-   c3 = Connector(g3,g4)
-   print(g4.getOutput())
+    def getOutput(self):
+        self.sumout.setInput()
+        self.carry.pinA = self.sumout.pinA
+        self.carry.pinB = self.sumout.pinB
+        sum_result = self.sumout.getOutput()
+        carry_result = self.carry.getOutput()
 
-main()
+        return sum_result, carry_result
+
+#Now extend that circuit and implement an 8 bit full-adder.
+
+
+
+ha = HalfAdder()
+print(ha.getOutput())
+
