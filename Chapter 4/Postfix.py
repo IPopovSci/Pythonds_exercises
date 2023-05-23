b24314bbddb16a250c529d8e094f9e233459caba
IvanPopAudio <72165662+IvanPopAudio@users.noreply.github.com>
2023-05-23 16:35:27 -0400

    Advanced infix to postfix, no manual brackets

--- Chapter 4/Postfix.py
+++ Chapter 4/Postfix.py
@@ -5,8 +5,7 @@ def IntoPost(infix_expression):
     opstack = Stack()
     output = []
     operands = ['*','/','+','-']
-    infix_expression = list(infix_expression)
-    error = False
+    infix_expression = list(infix_expression.replace(' ',''))
     switch = False
     for token in infix_expression:
         if token=='(':
@@ -35,6 +34,37 @@ def IntoPost(infix_expression):
             print('Something went horribly wrong')
     return output
 
-print(IntoPost('((A*B)+(C*D))'))
+def IntoPostImproved(infix_expression):
+    prec = {}
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec["-"] = 2
+    prec["("] = 1
+    opstack = Stack()
+    output = []
+    operands = ['*','/','+','-']
+    infix_expression = list(infix_expression.replace(' ',''))
+    for token in infix_expression:
+        if token=='(':
+            opstack.push(token)
+        elif token==')' and not opstack.isEmpty():
+            topToken = opstack.pop()
+            while topToken != '(':
+                output.append(topToken)
+                topToken = opstack.pop()
+        elif token in operands:
+            while (not opstack.isEmpty()) and \
+               (prec[opstack.peek()] >= prec[token]):
+                  output.append(opstack.pop())
+            opstack.push(token)
 
+        elif isinstance(token,str):
+            output.append(token)
+        else:
+            print('Something went horribly wrong')
+    while not opstack.isEmpty():
+        output.append(opstack.pop())
+    return output
 
+print(print(IntoPostImproved("( A + B ) * C - ( D - E ) * ( F + G )")))
