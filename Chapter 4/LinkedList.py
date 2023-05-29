9449193775bb4dc6462a6b33f097b9f006477b48
IvanPopAudio <72165662+IvanPopAudio@users.noreply.github.com>
2023-05-29 15:01:46 -0400

    Implement some parts of UnorderedList

--- Chapter 4/LinkedList.py
+++ Chapter 4/LinkedList.py
@@ -15,10 +15,28 @@ class Node:
     def setNext(self, newnext):
         self.next = newnext
 
+
 class UnorderedList:
 
     def __init__(self):
         self.head = None
+        self.tail = None
+        self.length = 0
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.getNext()
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.getNext()
 
     def isEmpty(self):
         return self.head == None
@@ -27,3 +45,46 @@ class UnorderedList:
         temp = Node(item)
         temp.setNext(self.head)
         self.head = temp
+        self.length+=1
+
+    def size(self):
+        return self.length
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        if self.length==0:
+            raise Exception("Can't delete last item in an empty list")
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+        self.length-=1
+    #Implement append, insert, index, and pop
+    #O(n) append:
+    def append(self,item):
+        current = self.head
+        item_node = Node(item)
+        if current:
+            while current.getNext() != None:
+                current=current.getNext()
+            current.setNext(item_node)
+        else:
+            current.setNext(item_node)
