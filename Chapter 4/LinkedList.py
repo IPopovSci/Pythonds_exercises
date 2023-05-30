fecf30a02c0fde6302e8dfb25d7e50f13a8c6310
IvanPopAudio <72165662+IvanPopAudio@users.noreply.github.com>
2023-05-30 10:52:51 -0400

    Implement tail for linked list, modify add,remove; add append method.

--- Chapter 4/LinkedList.py
+++ Chapter 4/LinkedList.py
@@ -3,6 +3,10 @@ class Node:
         self.data = initdata
         self.next = None
 
+    # def __repr__(self):
+    #     return f'Node object: (Data:{self.data},next:{self.next})'
+    def __str__(self):
+        return str(self.data)
     def getData(self):
         return self.data
 
@@ -23,29 +27,36 @@ class UnorderedList:
         self.tail = None
         self.length = 0
 
-    def __repr__(self):
+    def __str__(self):
         node = self.head
         nodes = []
         while node is not None:
             nodes.append(node.data)
             node = node.getNext()
+        if self.tail:
+            nodes.append(self.tail.data)
         nodes.append("None")
-        return " -> ".join(nodes)
-
+        return str(nodes)
     def __iter__(self):
         node = self.head
         while node is not None:
             yield node
             node = node.getNext()
+        yield self.tail
 
     def isEmpty(self):
         return self.head == None
 
     def add(self, item):
+
         temp = Node(item)
-        temp.setNext(self.head)
-        self.head = temp
-        self.length+=1
+        if self.tail==None:
+            self.tail = temp
+        else:
+            temp.setNext(self.head)
+            self.head = temp
+            self.length+=1
+
 
     def size(self):
         return self.length
@@ -66,20 +77,26 @@ class UnorderedList:
         found = False
         if self.length==0:
             raise Exception("Can't delete last item in an empty list")
-        while not found:
+        while not found and current!=None:
             if current.getData() == item:
                 found = True
-            else:
+            elif current.getNext()!=None:
                 previous = current
                 current = current.getNext()
+            elif self.tail.getData()==item:
+                found=True
+                self.tail=current
+            elif not found:
+                raise Exception('Item not found')
         if previous == None:
             self.head = current.getNext()
-        else:
+        elif found and self.tail:
             previous.setNext(current.getNext())
+
         self.length-=1
     #Implement append, insert, index, and pop
     #O(n) append:
-    def append(self,item):
+    def append_on(self,item):
         current = self.head
         item_node = Node(item)
         if current:
@@ -88,3 +105,46 @@ class UnorderedList:
             current.setNext(item_node)
         else:
             current.setNext(item_node)
+        self.length+=1
+    #O(1) append:
+    def append(self,item):
+        item_node = Node(item)
+        self.tail=item_node
+        self.length+=1
+    # def insert(self,position,item):
+    #     current = self.head
+    #     item_node = Node(item)
+    #     list_pos=0
+    #     if current:
+    #         while list_pos != position:
+    #             list_pos+=1
+    #             if list_pos != position:
+    #                 current = current.getNext()
+    #             else:
+    #                 #future_node = current.getNext()
+    #                 self.head=item_node
+    #                 #self.head = future_node
+    #
+    #     #current.setNext(item_node)
+
+
+
+
+mylist = UnorderedList()
+mylist.add(31)
+mylist.add(77)
+mylist.add(17)
+mylist.add(31)
+mylist.add(93)
+mylist.add(26)
+mylist.add(54)
+print(mylist)
+mylist.remove(31)
+print('head',mylist.head)
+mylist.remove(31)
+mylist.remove(54)
+#mylist.append(100)
+#mylist.insert(1,27)
+print(mylist)
+print('head',mylist.head)
+print('tail',mylist.tail)
